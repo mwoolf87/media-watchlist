f52daa2820d10028bd7120146876389fd1def480
julszymanski <85003886+julszymanski@users.noreply.github.com>
2021-11-20 16:00:18 -0500

    created migration

---
+++ back-end/migrations/20211120205912-create-user.js
@@ -0,0 +1,42 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('user', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      firstName: {
+        type: Sequelize.STRING
+      },
+      lastName: {
+        type: Sequelize.STRING
+      },
+      password: {
+        type: Sequelize.STRING
+      },
+      favoriteBooks: {
+        type: Sequelize.STRING
+      },
+      favoriteFlicks: {
+        type: Sequelize.STRING
+      },
+      favoriteSongs: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('user');
+  }
+};
