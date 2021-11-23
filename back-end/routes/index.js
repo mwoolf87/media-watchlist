37ba02bac875994052e60232a715371c8354b75e
julszymanski <85003886+julszymanski@users.noreply.github.com>
2021-11-22 19:38:02 -0500

    login modal

--- back-end/routes/index.js
+++ back-end/routes/index.js
@@ -1,9 +1,17 @@
-var express = require('express');
+const express = require('express');
+const app = express();
+
+const server = http.createServer(app);
+
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/login', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
+
+//pgp and db requirements
+const pgp = require("pg-promise")();
+const db = pgp("postgres://@127.0.0.1:5432/capstone");
