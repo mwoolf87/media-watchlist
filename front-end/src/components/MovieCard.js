0a0bad36295b38753990a2990c72bbe94590b49e
mwoolf87 <michael.j.woolf@gmail.com>
2021-11-30 20:33:18 -0500

    commiting prop changes

--- front-end/src/components/MovieCard.js
+++ front-end/src/components/MovieCard.js
@@ -1,27 +1,29 @@
-import React from 'react';
-import { Card, Button } from 'react-bootstrap';
-import { useDispatch } from 'react-redux';
-import { setMovieData } from '../redux/actions/movieData-actions';
+import React from "react";
+import { Card, Button } from "react-bootstrap";
+import { useDispatch } from "react-redux";
+import { setMovieData } from "../redux/actions/movieData-actions";
 
 export default function MovieCard(props) {
-    // defining dispatch to use on line 22
-    const dispatch = useDispatch();
-    //deconstructing props here
-    const { Title, Year, Poster } = props.movie
-    return (
-        <div className="movie-card-container">
-            {/* copied this card syntax from react-bootstrap */}
-            <Card className="movie-card" style={{ width: '18rem' }}>
-                {/* using the the Poster variable defined from our prop deconstruction to reference a src="" for the image */}
-                <Card.Img variant="top" src={Poster} />
-                <Card.Body>
-                    {/* calling the title and year of the movie, within this react-bootstrap card */}
-                    <Card.Title>{Title}</Card.Title>
-                    <Card.Title>{Year}</Card.Title>
-                    {/* calling dispatch to set the state of our watchlist data in reducer */}
-                    <Button onClick={() => dispatch(setMovieData(props.movie))}>Add to Watchlist</Button>
-                </Card.Body>
-            </Card>
-        </div>
-    )
+  // defining dispatch to use on line 22
+  const dispatch = useDispatch();
+  //deconstructing props here
+  const { Title, Year, Poster } = props.data;
+  return (
+    <div className="movie-card-container">
+      {/* copied this card syntax from react-bootstrap */}
+      <Card className="movie-card" style={{ width: "18rem" }}>
+        {/* using the the Poster variable defined from our prop deconstruction to reference a src="" for the image */}
+        <Card.Img variant="top" src={Poster} />
+        <Card.Body>
+          {/* calling the title and year of the movie, within this react-bootstrap card */}
+          <Card.Title>{Title}</Card.Title>
+          <Card.Title>{Year}</Card.Title>
+          {/* calling dispatch to set the state of our watchlist data in reducer */}
+          <Button onClick={() => dispatch(setMovieData(props.data))}>
+            Add to Watchlist
+          </Button>
+        </Card.Body>
+      </Card>
+    </div>
+  );
 }
