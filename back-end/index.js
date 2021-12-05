503d1d3a290d857f7082cc5cbdf5d85a53ae8515
Scott Henderson <scott.mc.henderson@gmail.com>
2021-12-05 09:45:37 -0800

    Working local hosted postgres server

--- back-end/index.js
+++ back-end/index.js
@@ -1,32 +1,68 @@
+/* Boilerplate Express */
+
 const http = require('http');
 const hostname = '127.0.0.1';
-const port = 4000;
+const port = process.env.PORT || 4000;
+
 const express = require('express');
 const app = express();
-
+app.use(express.json());
 const server = http.createServer(app);
 
-/* Import Models*/
+/* Import Database Models*/
 const Sequelize = require('sequelize');
-const Users = require('./models/users');
+const { User } = require('./models');
 
-/* Test */
+/* Connect to the database */
+const db = new Sequelize('database_development', 'root', null, {
+    host: 'localhost',
+    dialect: 'postgres',
+  });
+
+/* Middleware */
+const morgan = require('morgan');
+const logger = morgan('tiny');
+app.use(logger);
+
+/* Security */
+const helmet = require('helmet');
+app.use(helmet());
+
+// Clear issues up with CORS to remove any Access Control Errors
+const cors = require('cors');
+app.use(cors());
+app.set('view engine', 'html');
+app.all('*', (req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header("Access-Control-Allow-Headers", "Content-Type,Content-Length, Authorization, Accept,X-Requested-With");
+    res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json')
+    // console.log(`${req.method} ${req.path}`);
+    next();
+  });
+
+/* Test DB */
+db.authenticate()
+  .then(() => console.log('Database Connected'))
+  .catch(err => console.log('Error' + err))
+
+/* Test Express */
 app.get('/', (req, res) => {
     res.send('Hello World');
 });
 
-/* Create */
+/* Create User */
 app.post('/users', async (req, res) => {
     // req.body contains an Object with firstName, lastName, email
-    const { firstName, lastName, email } = req.body;
+    const { firstName, lastName, email, hash, favoriteBooks, favoriteFlicks, favoriteApps } = req.body;
     const newUser = await User.create({
-        firstName,
-        lastName,
-        email,
-        hash,
-        favoriteBooks,
-        favoriteFlicks,
-        favoriteApps
+        firstName: req.body.firstName, 
+        lastName: req.body.lastName,
+        email: req.body.email,
+        hash: req.body.hash,
+        favoriteBooks: req.body.favoriteBooks,
+        favoriteFlicks: req.body.favoriteFlicks,
+        favoriteApps: req.body.favoriteApps
     });
     
     // Send back the new user's ID in the response:
@@ -35,37 +71,58 @@ app.post('/users', async (req, res) => {
     });
 })
 
-/* Read */
+/* Read All Users */ 
 app.get('/users', async (req, res) => {
-    const users = await Users.findAll();
-    res.json(users);
+    const users = await User.findAll();
+    res.status(200).json(users);
 });
 
+// Read user by ID
+app.get('/users/:id', async (req, res) => {
+    try{
+        const oneUser = await User.findByPk(req.params.id);
+        res.json(oneUser);
+    } catch (e) {
+        console.log(e);
+        res.status(404).json({
+            message: 'User id not found'
+        });
+    }
+  });
+
+
 /* Update */
-app.post('/users/:id', async (req, res) => {
+app.put('/users/:id', async (req, res) => {
     const { id } = req.params;
-    
-    // Assuming that `req.body` is limited to
-    // the keys firstName, lastName, and email
-    const updatedUser = await User.update(req.body, {
-      where: {
-        id
-      }
-    });
-    
-    res.json(updatedUser);
+    const userFound = await User.findByPk(id);
+    console.log(userFound);
+    // Check that user exists in the database
+    if (userFound === null) {  // If the user doesn't exist
+        // console.log('User id not found');
+        res.status(400).json({message: "User id doesn't exist in database"});
+        } else {
+        // Assemble paramaters from ones that exist in the request
+        const updatedUser = await User.update(req.body, {where: {id} });
+        res.json(updatedUser);
+        };
 });
 
 /* Delete */
 app.delete('/users/:id', async (req, res) => {
-    const { id } = req.params;
-    const deletedUser = await URLSearchParams.destroy({
-        where: {
-            id
-        }
-    });
-    res.json(deletedUser);
-});
+    const {id} = req.params;
+    // Check that user exists in the database
+    const oneUser = await User.findByPk(id);
+    // If not found return "User id does not exist within database"
+    if (oneUser === null) {
+      // console.log('User not found');
+      res.status(400).json('User not found');
+    } else {
+    // If found then delete
+      const deletedUser = await User.destroy({ where: {id} });
+      // console.log('User deleted');
+      res.json(deletedUser);
+    }
+  });
 
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
--- back-end/models/user.js
+++ back-end/models/user.js
@@ -3,7 +3,7 @@ const {
   Model
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class Users extends Model {
+  class User extends Model {
     /**
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
@@ -13,7 +13,7 @@ module.exports = (sequelize, DataTypes) => {
       // define association here
     }
   };
-  Users.init({
+  User.init({
     firstName: DataTypes.STRING,
     lastName: DataTypes.STRING,
     email: DataTypes.STRING,
@@ -23,7 +23,7 @@ module.exports = (sequelize, DataTypes) => {
     favoriteApps: DataTypes.STRING
   }, {
     sequelize,
-    modelName: 'Users',
+    modelName: 'User',
   });
-  return Users;
+  return User;
 };
