8a894ab96298246f3dcc61ab4b1e9c5a220a2d94
Scott Henderson <scott.mc.henderson@gmail.com>
2021-12-05 21:21:10 -0800

    updated backed express, fixing merge from main issues

--- back-end/index.js
+++ back-end/index.js
@@ -1,61 +1,141 @@
-// server/index.js
+/* Boilerplate Express */
+
 const path = require('path');
 const express = require("express");
+const hostname = '127.0.0.1';
 const PORT = process.env.PORT || 4000;
-
 const app = express();
+app.use(express.json());
 
-const db = require('./models/index.js');
 const Sequelize = require('sequelize');
 const { User } = require('./models');
+const http = require('http');
+const server = http.createServer(app);
 
-const Pool = require("pg").Pool;
-require("dotenv").config();
-const isProduction = process.env.NODE_ENV === "production";
-const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`;
-const pool = new Pool({
-    connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
-    ssl: {
-        rejectUnauthorized: false,
-    },
+const db = new Sequelize('database_development', 'root', null, {
+  host: 'localhost',
+  dialect: 'postgres',
 });
-module.exports = pool;
 
-// Have Node serve the files for our built React app
-app.use(express.static(path.resolve(__dirname, '../client/build')));
+/* Test DB */
+db.authenticate()
+  .then(() => console.log('Database Connected'))
+  .catch(err => console.log('Error' + err))
 
-app.get("/api", (req, res) => {
-    res.json({ message: "Hello from server!" });
+/* Middleware */
+const morgan = require('morgan');
+const logger = morgan('tiny');
+app.use(logger);
+
+/* Security */
+const helmet = require('helmet');
+app.use(helmet());
+
+// Clear issues up with CORS to remove any Access Control Errors
+const cors = require('cors');
+app.use(cors());
+app.set('view engine', 'html');
+app.all('*', (req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header("Access-Control-Allow-Headers", "Content-Type,Content-Length, Authorization, Accept,X-Requested-With");
+    res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json')
+    // console.log(`${req.method} ${req.path}`);
+    next();
   });
-
-
-// All other GET requests not handled before will return our React app
-app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
-  });
-
 
 // Test get
-app.get('/test', async (req, res) => {
-    res.status(200).send("Hello World");
-  });
-
-// READ all Users
-app.get('/users', async (req, res) => {
-    res.setHeader("Content-Type","application/json");
-    const allUsers = await User.findAll();
-    console.log(allUsers);
-    res.status(200).send(allUsers);
-  });
-
-//Get users by lastName
-app.get('/users/by-last-name', async (req, res) => {
-    const users = await User.findAll({
-        attributes: ['lastName']
-    });
-    res.json(users);
+app.get('/', (req, res) => {
+  res.send('Hello from Express!');
 });
 
-app.listen(PORT, () => {
-    console.log(`Server listening on ${PORT}`);
+
+  /* Read All Users */ 
+app.get('/users', async (req, res) => {
+  const users = await User.findAll();
+  res.status(200).json(users);
+});
+
+/* Read User by Id */
+app.get('/users/:id', async (req, res) => {
+  try{
+      const oneUser = await User.findByPk(req.params.id);
+      res.json(oneUser);
+  } catch (e) {
+      console.log(e);
+      res.status(404).json({
+          message: 'User not found'
+      });
+  }
+});
+
+
+/* Create User */
+app.post('/users', async (req, res) => {
+  // req.body contains an Object with firstName, lastName, email
+  const { firstName, lastName, email, hash, favoriteBooks, favoriteFlicks, favoriteApps } = req.body;
+  const newUser = await User.create({
+      firstName: req.body.firstName, 
+      lastName: req.body.lastName,
+      email: req.body.email,
+      hash: req.body.hash,
+      favoriteBooks: req.body.favoriteBooks,
+      favoriteFlicks: req.body.favoriteFlicks,
+      favoriteApps: req.body.favoriteApps
   });
+   // Send back the new user's ID in the response:
+   res.json({
+    id: newUser.id
+  });
+})
+
+// Read user by ID
+app.get('/users/:id', async (req, res) => {
+  try{
+      const oneUser = await User.findByPk(req.params.id);
+      res.json(oneUser);
+  } catch (e) {
+      console.log(e);
+      res.status(404).json({
+          message: 'User id not found'
+      });
+  }
+});
+
+
+/* Update */
+app.put('/users/:id', async (req, res) => {
+  const { id } = req.params;
+  const userFound = await User.findByPk(id);
+  console.log(userFound);
+  // Check that user exists in the database
+  if (userFound === null) {  // If the user doesn't exist
+      // console.log('User id not found');
+      res.status(400).json({message: "User id doesn't exist in database"});
+      } else {
+      // Assemble paramaters from ones that exist in the request
+      const updatedUser = await User.update(req.body, {where: {id} });
+      res.json(updatedUser);
+      };
+});
+
+/* Delete */
+app.delete('/users/:id', async (req, res) => {
+  const {id} = req.params;
+  // Check that user exists in the database
+  const oneUser = await User.findByPk(id);
+  // If not found return "User id does not exist within database"
+  if (oneUser === null) {
+    // console.log('User not found');
+    res.status(400).json('User not found');
+  } else {
+  // If found then delete
+    const deletedUser = await User.destroy({ where: {id} });
+    // console.log('User deleted');
+    res.json(deletedUser);
+  }
+});
+
+server.listen(PORT, hostname, () => {
+  console.log(`Server running at http://${hostname}:${PORT}/`);
+});
