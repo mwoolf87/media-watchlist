240b86b26dc40b0669c4a62b4143256369f739c8
julszymanski <85003886+julszymanski@users.noreply.github.com>
2021-12-07 19:29:32 -0500

    bcrypt hash success

--- back-end/routes/loginRegistration.js
+++ back-end/routes/loginRegistration.js
@@ -21,40 +21,40 @@ const sequelize = new Sequelize('sqlite::memory:', {
 router.post('/verify', async (req, res) => {
   try { 
     const bodyEmail = req.body.email;
-  await User.findOne({where: { email: bodyEmail } }).then (foundUser => { // Check email against database
-    if (foundUser !== null) { // If user was found in the database
-      if (req.body.password === foundUser.hash) { // If plaintext password hash matches database hash
-        // console.log('Database hash matches plaintext password')
-        res.status(200).send({message : 'Login successful!', login: true});
-      } else { // 
-        res.status(401).send({message: 'Username or password incorrect', login: false});
-      }
-    } else {
-      res.status(401).send({message: 'Sorry that user name does not exist', login: false});
-    }
-  }); } catch (error) {
-    console.error(error.message);
-    res.status(500).send("Server Error");
-  }
+    await User.findOne({where: { email: bodyEmail } }).then (foundUser => { // Check email against database
+      if (foundUser !== null) { // If user was found in the database
+        if (checkPassword(req.body.password, foundUser.hash)) { // If plaintext password hash matches database hash
+            // console.log('Database hash matches plaintext password')
+            res.status(200).send({message : 'Login successful!', login: true});
+        } else { // 
+            res.status(401).send({message: 'Username or password incorrect', login: false});
+        }
+        } else {
+        res.status(401).send({message: 'Sorry that user name does not exist', login: false});
+        }
+    }); } catch (error) {
+        console.error(error.message);
+        res.status(500).send("Server Error");
+        }
 });
 
 router.post('/register', async (req, res) => {
   try {
-  const bodyEmail = req.body.email;
-  await User.findOne({where: { email: bodyEmail } }).then (foundUser => { // Check email against database
-    if (foundUser !== null) { // If user was found in the database
-      // console.log('user already exists');
-      res.status(401).send({message: "Email already has been registered , please try another", registration: false});
-    } else {
-      // console.log('DEBUG: email not registered');
-    //   requestHash = hashPassword(req.body.password);
-      const newUser = User.create({ // Pass info through postman
-          firstName: req.body.firstName, 
-          lastName: req.body.lastName,
-          email: req.body.email,
-          hash: req.body.password});
-        res.status(201).json({id: newUser.id, message: 'New User created!', registration: true});
-      }
+    const bodyEmail = req.body.email;
+    await User.findOne({where: { email: bodyEmail } }).then (foundUser => { // Check email against database
+        if (foundUser !== null) { // If user was found in the database
+        // console.log('user already exists');
+        res.status(401).send({message: "Email already has been registered , please try another", registration: false});
+        } else {
+        // console.log('DEBUG: email not registered');
+        requestHash = hashPassword(req.body.password);
+        const newUser = User.create({ // Pass info through postman
+            firstName: req.body.firstName, 
+            lastName: req.body.lastName,
+            email: req.body.email,
+            hash: requestHash});
+            res.status(201).json({id: newUser.id, message: 'New User created!', registration: true});
+        }
     }); } catch (error) {
     console.error(error.message);
     res.status(500).send("Server Error");
@@ -70,17 +70,17 @@ module.exports = router;
 
 // bcrypt - Encrypt Passwords
 const bcrypt = require('bcrypt');
-// const saltRounds = 10;
+const saltRounds = 10;
 
-// function hashPassword(password) {
-//   const hash = bcrypt.hashSync(password, saltRounds);
-//   // console.log('Plain text: ', password);
-//   // console.log('Hash:', hash);
-//   return hash
-// }
+function hashPassword(password) {
+  const hash = bcrypt.hashSync(password, saltRounds);
+  // console.log('Plain text: ', password);
+  // console.log('Hash:', hash);
+  return hash
+}
 
-// function checkPassword(password, hash) {
-//   const result = bcrypt.compareSync(password, hash) 
-//   // console.log('Password matches hash', result);
-//   return result
-// }
+function checkPassword(password, hash) {
+  const result = bcrypt.compareSync(password, hash) 
+  // console.log('Password matches hash', result);
+  return result
+}
