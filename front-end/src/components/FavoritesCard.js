15d88f339113222bfea5835f27a60d7cbf295c76
julszymanski <85003886+julszymanski@users.noreply.github.com>
2021-12-11 10:51:48 -0500

    flicks table updated and favoriteCard

--- front-end/src/components/FavoritesCard.js
+++ front-end/src/components/FavoritesCard.js
@@ -1,9 +1,13 @@
 import React, { useEffect } from "react";
+import { Card, Button } from "react-bootstrap";
+import { useDispatch } from "react-redux";
 import axios from 'axios';
 import { useState } from "react";
 
 export default function FavoritesCard() {
     const [favorites, setFavorites] = useState();
+    const dispatch = useDispatch();
+
 
   const getFavorites = () => {
       axios
@@ -16,26 +20,41 @@ export default function FavoritesCard() {
       getFavorites();
   },[]);
 
+  // <div>
+  //       { favorites && <div>
+  //       <div>Title: {favorites.title}</div>
+  //       <div>Poster: <img src = {favorites.poster} alt = "test" /> </div>
+  //       <div>RunTime: {favorites.runTime}</div>
+  //       <div>Year: {favorites.year}</div>
+  //       <div>Director: {favorites.director}</div>
+  //       <div>Rated: {favorites.rated}</div>
+  //       <div>imdbRating: {favorites.imdbRating}</div>
 
-  // title: 'Croupier',
-  // poster: "./images/croupier.jpeg",
-  // runTime: '1h 34m',
-  // year: 1998,
-  // director: "Mike Hodges",
-  // rating: "NR",
-  // UserId: 1,
+  // </div>
 
   return (
-      <div>
-        { favorites && <div>
-        <div>Title: {favorites.title}</div>
-        <div>Poster: <img src = {favorites.poster} alt = "test" /> </div>
-        <div>RunTime: {favorites.runTime}</div>
-        <div>Year: {favorites.year}</div>
-        <div>Director: {favorites.director}</div>
-        <div>Rating: {favorites.rating}</div>
-        </div>
-         }
-      </div>
+    <div>
+    { favorites &&
+    <div className="movie-card-container">
+    {/* copied this card syntax from react-bootstrap */}
+    <Card className="movie-card" style={{ width: "18rem" }}>
+      {/* using the the Poster variable defined from our prop deconstruction to reference a src="" for the image */}
+      <Card.Img
+        variant="top"
+        src={favorites.poster}
+      />
+      <Card.Body>
+        {/* calling the title and year of the movie, within this react-bootstrap card */}
+        <Card.Title>{favorites.title}</Card.Title>
+        <Card.Title>{favorites.released}</Card.Title>
+
+        {/* calling dispatch to set the state of our watchlist data in reducer */}
+        <Button id="moreinfobutton" variant="warning">
+          <a href="/moreinfo">More Info</a>
+        </Button>
+      </Card.Body>
+    </Card>
+  </div> }
+</div>
   );
 }
