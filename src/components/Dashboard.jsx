d0427c1cfe4501e9515dd62653351d74c6dc47a9
Scott Henderson <scott.mc.henderson@gmail.com>
2021-12-15 14:13:43 -0800

    dashboard setup

---
+++ src/components/Dashboard.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Dashboard() {
+    return (
+        <div>
+            <h4>User "Username Here" Dashboard</h4>
+            <div>First: "{first}" </div>
+            <div>Last: "{last}"</div>
+            <h4>Updated Password</h4>
+            <button>Click Me</button>
+
+        </div>
+    )
+}
+
+export default Dashboard
