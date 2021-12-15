894cbe468c54d695fcba4412916c53d015906f18
mwoolf87 <michael.j.woolf@gmail.com>
2021-12-14 21:56:19 -0500

    search result yielding error message

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,11 +1,19 @@
 import React, { useState } from "react";
-import { Card, Button, Modal, Container, Row, Col, Image } from "react-bootstrap";
+import {
+  Card,
+  Button,
+  Modal,
+  Container,
+  Row,
+  Col,
+  Image
+} from "react-bootstrap";
 import { useDispatch } from "react-redux";
 import { setApiData } from "../redux/actions/movieData-actions";
 import { setModalData } from "../redux/actions/detailedMovieData-actions";
-import imdblogo from './Images/imdb.png';
-import csmlogo from './Images/csm.png';
-import justwatch from './Images/justwatch-square.png'
+import imdblogo from "./Images/imdb.png";
+import csmlogo from "./Images/csm.png";
+import justwatch from "./Images/justwatch-square.png";
 import "../CSS/MovieCard.css";
 
 export default function MovieCard(props) {
@@ -24,13 +32,29 @@ export default function MovieCard(props) {
 
   const CAT_404 = "https://http.cat/404";
 
-  const { Director, Genre, imdbRating, imdbID, Language, Metascore, Plot, Poster, Release, Rated, Runtime, Title, Year} = props.movie;
-  /* Define custom urls */ 
-  const parentsGuideURL = "https://www.imdb.com/title/" + imdbID + "/parentalguide";
-  
+  const {
+    Director,
+    Genre,
+    imdbRating,
+    imdbID,
+    Language,
+    Metascore,
+    Plot,
+    Poster,
+    Release,
+    Rated,
+    Runtime,
+    Title,
+    Year
+  } = props.movie;
+  /* Define custom urls */
+  const parentsGuideURL =
+    "https://www.imdb.com/title/" + imdbID + "/parentalguide";
+
   // Dash title (i.e. Lord of the Rings: Return -> Lord-of-the-Rings-Return)
-  const dashedTitle = (Title.replace(/:+/g, '')).replace(/\s+/g, '-');
-  const commonSenseURL = "https://www.commonsensemedia.org/movie-reviews/" + dashedTitle;
+  const dashedTitle = Title.replace(/:+/g, "").replace(/\s+/g, "-");
+  const commonSenseURL =
+    "https://www.commonsensemedia.org/movie-reviews/" + dashedTitle;
   const justWatchURL = "https://www.justwatch.com/us/movie/" + dashedTitle;
 
   return (
@@ -62,49 +86,53 @@ export default function MovieCard(props) {
       </Card>
       {modalShow && <Modal />}
 
-      <Modal show={modalShow}  onHide={handleClose}>
+      <Modal show={modalShow} onHide={handleClose}>
         <Modal.Header closeButton>
           <Modal.Title>{Title}</Modal.Title>
         </Modal.Header>
-          <Row>
-            <Col>Director: {Director}</Col>
-            <Col>Runtime: {Runtime}</Col>
-            <Col>Year: {Year}</Col>
-            <Col>Language: {Language}</Col>
-            <Col>Rating: {Rated}</Col>
-            <Col>Metascore: {Metascore}</Col>
-            <Col>imdbRating: {imdbRating}</Col>
-          </Row>
-          <Row>
-            <Col>Genre: {Genre}</Col>
-          </Row>
+        <Row>
+          <Col>Director: {Director}</Col>
+          <Col>Runtime: {Runtime}</Col>
+          <Col>Year: {Year}</Col>
+          <Col>Language: {Language}</Col>
+          <Col>Rating: {Rated}</Col>
+          <Col>Metascore: {Metascore}</Col>
+          <Col>imdbRating: {imdbRating}</Col>
+        </Row>
+        <Row>
+          <Col>Genre: {Genre}</Col>
+        </Row>
         <Modal.Body>{Plot}</Modal.Body>
-          <Row>
-            <Col>
-              <a href={parentsGuideURL} target = "_blank" rel="noreferrer noopener">
-                <Image className = "imdblogo" src = {imdblogo} alt = "imdb_logo" />
-                <div>Parents Guide</div>
-              </a>
-            </Col>
-            <Col>
-              <a href={commonSenseURL} target = "_blank" rel="noreferrer noopener">
-                <Image src = {csmlogo} alt = "csm_logo" />
-                <div>Common Sense Media</div>
-              </a>
-            </Col>
-            <Col>
-              <a href={justWatchURL} target = "_blank" rel="noreferrer noopener">
-                <Image className = "imdblogo" src = {justwatch} alt = "just_watch_logo" />
-                <div>Stream</div>
-              </a>
-            </Col>
-          </Row>
+        <Row>
+          <Col>
+            <a href={parentsGuideURL} target="_blank" rel="noreferrer noopener">
+              <Image className="imdblogo" src={imdblogo} alt="imdb_logo" />
+              <div>Parents Guide</div>
+            </a>
+          </Col>
+          <Col>
+            <a href={commonSenseURL} target="_blank" rel="noreferrer noopener">
+              <Image src={csmlogo} alt="csm_logo" />
+              <div>Common Sense Media</div>
+            </a>
+          </Col>
+          <Col>
+            <a href={justWatchURL} target="_blank" rel="noreferrer noopener">
+              <Image
+                className="imdblogo"
+                src={justwatch}
+                alt="just_watch_logo"
+              />
+              <div>Stream</div>
+            </a>
+          </Col>
+        </Row>
         <Modal.Footer>
           <Button variant="secondary" onClick={handleClose}>
             Close
           </Button>
-          <Button variant="primary" onClick={handleClose}>
-            Save Changes
+          <Button onClick={() => dispatch(setApiData(props.movie))}>
+            Add to Watchlist
           </Button>
         </Modal.Footer>
       </Modal>
