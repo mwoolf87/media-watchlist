0d718a8855e401bbb04f527d24f6efce2f8ffb22
Scott Henderson <scott.mc.henderson@gmail.com>
2021-12-15 16:00:34 -0800

    fix dashboard draft

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -4,8 +4,8 @@ function Dashboard() {
     return (
         <div>
             <h4>User "Username Here" Dashboard</h4>
-            <div>First: "{first}" </div>
-            <div>Last: "{last}"</div>
+            <div>First: "first" </div>
+            <div>Last: "last"</div>
             <h4>Updated Password</h4>
             <button>Click Me</button>
 
