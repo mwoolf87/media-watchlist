875aeb9426271264e09cbdfd9970b34d2bf496a9
Scott Henderson <scott.mc.henderson@gmail.com>
2022-01-11 16:03:48 -0800

    contact progress

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,100 +1,87 @@
-import React from "react";
-import { useForm } from 'react-hook-form';
-import emailjs from 'emailjs-com';
-import { ToastContainer, toast } from 'react-toastify';
+import React, {useState} from "react";
 import 'react-toastify/dist/ReactToastify.min.css';
-import{ init } from 'emailjs-com';
 init(process.env.USER_ID);
 
+/* https://javascript.plainenglish.io/sending-email-in-react-application-with-nodemailer-afcef6906608
+*/
+
 const Contact = () => {
-  const {
-    register,
-    handleSubmit,
-    reset,
-    formState: { errors }
-  } = useForm();
-  
-  // Function that displays a success toast on bottom right of the page when form submission is successful
-  /* const toastifySuccess = () => {
-    toast('Form sent!', {
-      position: 'bottom-right',
-      autoClose: 5000,
-      hideProgressBar: true,
-      closeOnClick: true,
-      pauseOnHover: true,
-      draggable: false,
-      className: 'submit-feedback success',
-      toastId: 'notifyToast'
-    });
-  }; */
-  
-  // Function called on submit that uses emailjs to send email of valid contact form
-  const onSubmit = async (data) => {
-    data.preventDefault();
-    const { name, email, subject, message } = data;
-    console.log('Name: ', name);
-    console.log('Email: ', email);
-    console.log('Subject: ', subject);
-    console.log('Message: ', message);
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
+
+  const submitRequest = async (e) => {
+    e.preventDefault();
+    console.log({ email, message });
+    const response = await fetch("/access", { 
+      method: 'POST', 
+      headers: { 
+          'Content-type': 'application/json'
+      }, 
+      body: JSON.stringify({email, message}) 
+    }); 
+      const resData = await response.json(); 
+      if (resData.status === 'success'){
+        alert("Message Sent."); 
+        this.resetForm()
+      }else if(resData.status === 'fail'){
+          alert("Message failed to send.")
+      }
   };
 
   return (
-    <div className='ContactForm'>
-      <div className='container'>
-        <div className='row'>
-          <div className='col-12 text-center'>
-            <div className='contactForm'>
-              <form id='contact-form' noValidate>
-                {/* Row 1 of form */}
-                <div className='row formRow'>
-                  <div className='col-6'>
-                    <input
-                      type='text'
-                      name='name'
-                      className='form-control formInput'
-                      placeholder='Name'
-                    ></input>
-                  </div>
-                  <div className='col-6'>
-                    <input
-                      type='email'
-                      name='email'
-                      className='form-control formInput'
-                      placeholder='Email address'
-                    ></input>
-                  </div>
-                </div>
-                {/* Row 2 of form */}
-                <div className='row formRow'>
-                  <div className='col'>
-                    <input
-                      type='text'
-                      name='subject'
-                      className='form-control formInput'
-                      placeholder='Subject'
-                    ></input>
-                  </div>
-                </div>
-                {/* Row 3 of form */}
-                <div className='row formRow'>
-                  <div className='col'>
-                    <textarea
-                      rows={3}
-                      name='message'
-                      className='form-control formInput'
-                      placeholder='Message'
-                    ></textarea>
-                  </div>
-                </div>
-                <button className='submit-btn' type='submit'>
-                  Submit
-                </button>
-              </form>
-            </div>
-          </div>
+    <div>
+    <div className="flex flex-col items-center justify-center bg-gray-200"></div>
+    <div className="w-full max-w-sm m-auto flex flex-col my-32">
+      <form
+        className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 border-gray-200 border"
+      >
+        <h2 className="text-2xl pt-6 pb-10 text-center font-medium text-gray-800">
+          Request for early access
+        </h2>
+        <div className="mb-4">
+          <label
+            className="block text-gray-700 text-sm font-bold mb-2"
+            htmlFor="Email"
+          >
+            Your Email
+          </label>
+          <input
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            type="text"
+            name="email"
+            placeholder="Email Address"
+            value='email'
+            required
+          />
         </div>
-      </div>
+        <div className="mb-4">
+          <label
+            className="block text-gray-700 text-sm font-bold mb-2"
+            htmlFor="message"
+          >
+            Message For Us
+          </label>
+          <textarea
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            name="message"
+            type="text"
+            placeholder="Tell us your purpose"
+            value='message'
+            required
+          />
+        </div>
+        <div className="flex items-center justify-between">
+          <button
+            className="bg-pink-500 hover:bg-pink-600 text-white font-bold py-2 px-4 mt-6 w-full rounded focus:outline-none focus:shadow-outline"
+            type="submit"
+          >
+            Send A Request
+          </button>
+        </div>
+      </form>
     </div>
+  </div>
+
   );
 };
 
