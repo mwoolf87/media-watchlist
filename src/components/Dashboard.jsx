0a6eb2754e7ff76abe2000e8493ecb596c0130d4
mwoolf87 <michael.j.woolf@gmail.com>
2022-01-11 19:25:55 -0500

    worked ternary to say guest user instead of null in both watchlist and dashboard

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -29,50 +29,57 @@ function Dashboard() {
 
   return (
     <div class="dashboard">
-      <Card style={{ width: "30rem" }}>
-        <Card.Body>
-          <Card.Title>
-            <h4>User Dashboard</h4>
-          </Card.Title>
-          <Card.Title>
-            <p>
-              Welcome, {String(local.getItem("first"))}{" "}
-              {String(local.getItem("last"))}
-            </p>
-          </Card.Title>
-          <Card.Title>
-            <div>Email: {String(local.getItem("email"))}</div>
-          </Card.Title>
-          <div>
-            <form class="input">
-              <label>
-                Update Email:
-                <input class="input-class" type="text" name="email" />
-              </label>
-              <input className="button-19 m-2" type="submit" value="Submit" />
-            </form>
-          </div>
-          {/* User clicks button, updates email to new email, show toast information */}
-          <div>
-            <form>
-              <label>
-                Update Password:
-                <input class="input-class" type="text" name="email" />
-              </label>
-              <input className="button-19 m-2" type="submit" value="Submit" />
-            </form>
-          </div>
-          {/* User click buttons, gives option to update password 
+      {String(local.getItem("first")) &&
+      String(local.getItem("last")) === "null" ? (
+        <h2 className="text-center">
+          Welcome, Guest! <br></br> Please Login to Edit your Credentials
+        </h2>
+      ) : (
+        <Card style={{ width: "30rem" }}>
+          <Card.Body>
+            <Card.Title>
+              <h4>User Dashboard</h4>
+            </Card.Title>
+            <Card.Title>
+              <p>
+                Welcome, {String(local.getItem("first"))}{" "}
+                {String(local.getItem("last"))}
+              </p>
+            </Card.Title>
+            <Card.Title>
+              <div>Email: {String(local.getItem("email"))}</div>
+            </Card.Title>
+            <div>
+              <form class="input">
+                <label>
+                  Update Email:
+                  <input class="input-class" type="text" name="email" />
+                </label>
+                <input className="button-19 m-2" type="submit" value="Submit" />
+              </form>
+            </div>
+            {/* User clicks button, updates email to new email, show toast information */}
+            <div>
+              <form>
+                <label>
+                  Update Password:
+                  <input class="input-class" type="text" name="email" />
+                </label>
+                <input className="button-19 m-2" type="submit" value="Submit" />
+              </form>
+            </div>
+            {/* User click buttons, gives option to update password 
             toast notifcation that user password has been updated */}
-          <button class="button-19 m-2">Delete Account</button>
-          {/* Delete account, delete user and then switch user to registration page
+            <button class="button-19 m-2">Delete Account</button>
+            {/* Delete account, delete user and then switch user to registration page
             show toast notifcation that account has been delete */}
-          <button class="button-19 m-2" onClick={logout}>
-            Log Out
-          </button>
-          {/* Logout from account */}
-        </Card.Body>
-      </Card>
+            <button class="button-19 m-2" onClick={logout}>
+              Log Out
+            </button>
+            {/* Logout from account */}
+          </Card.Body>
+        </Card>
+      )}
     </div>
   );
 }
