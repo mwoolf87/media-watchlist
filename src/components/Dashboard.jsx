06a7c908cec1d7ce11196e079875e8b8a97c60ac
Scott Henderson <scott.mc.henderson@gmail.com>
2022-01-19 16:55:25 -0800

    update dashboard console log

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -18,7 +18,7 @@ function Dashboard() {
     })
       .then(res => res.json())
       .then(data => {
-        console.log(data);
+        /* console.log(data); */
         if (data.message) {
           local.clear();
           navigate("/");
